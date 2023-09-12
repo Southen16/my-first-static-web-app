1a093606f89c92a0b46749024f7ab9961e1822ce
Southen16 <nama.southen16@gmail.com>
2023-09-11 17:12:46 -0700

    updated the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import lab from "./Lab1.jpg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <p>I did my homework!</p>
-      <img src={lab}/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
