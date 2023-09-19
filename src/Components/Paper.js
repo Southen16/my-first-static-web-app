a578e6d230650d9eea0d3d2f107b87c7566a5a0c
Southen16 <nama.southen16@gmail.com>
2023-09-18 19:00:13 -0700

    react updated

--- src/Components/Paper.js
+++ src/Components/Paper.js
@@ -1,21 +1,24 @@
 const Paper = () => {
     return (
         <div>
-            <h1>137 STAT. 50 Public Law 118-6—June 14, 2023</h1>
-            <p><b>Public Law 118–6</b><br />
-                <b>118th Congress</b></p>
-            <article>
-                <h2>An Act</h2>
-                <p>June 14, 2023</p>
-                <hr />
-                <p>[S. 777]</p>
-                <p>To increase, effective as of December 1, 2023, the rates of compensation for veterans<br />
-                with service-connected disabilities and the rates of dependency and indemnity<br />
-                compensation for the survivors of certain disabled veterans, and for other purposes.</p>
-                <p><i><b>Be it enacted by the Senate and House of Representatives of<br />
-                the United States of America in Congress assembled.</b></i></p>
+            <section>
+                <h1>137 STAT. 50 Public Law 118-6—June 14, 2023</h1>
+                <p><b>Public Law 118–6</b><br />
+                    <b>118th Congress</b></p>
+                <article>
+                    <h2>An Act</h2>
+                    <p>June 14, 2023</p>
+                    <hr />
+                    <p>[S. 777]</p>
+                    <p>To increase, effective as of December 1, 2023, the rates of compensation for veterans<br />
+                    with service-connected disabilities and the rates of dependency and indemnity<br />
+                    compensation for the survivors of certain disabled veterans, and for other purposes.</p>
+                    <p><i><b>Be it enacted by the Senate and House of Representatives of<br />
+                    the United States of America in Congress assembled.</b></i></p>
                 </article>
-                <div>
+            </section>
+
+            <div>
                 <article>
                     <p>Veterans’<br/>
                        Compensation<br/>
@@ -28,44 +31,50 @@ const Paper = () => {
                     of-Living Adjustment Act of 2023’’ or the ‘‘Veterans’ COLA Act <br/>
                     of 2023’’.</p>
                 </article>
-                </div>
-                <div>
-                    <article>
-                        <h3>SEC. 2. INCREASE IN RATES OF DISABILITY COMPENSATION AND<br/>
-                                DEPENDENCY AND INDEMNITY COMPENSATION. </h3>
-                                <p> 38 USC 1114<br/>
-                                    note. </p>
-                                    <p>
-                                    (a) RATE ADJUSTMENT.—Effective on December 1, 2023, the<br/>
-                                        Secretary of Veterans Affairs shall increase, in accordance with<br/>
-                                        subsection (c), the dollar amounts in effect on November 30, 2023,<br/>
-                                        for the payment of disability compensation and dependency and<br/>
-                                        indemnity compensation under the provisions specified in subsection<br/>
-(b). </p>
-<p>
-(b) AMOUNTS TO BE INCREASED.—The dollar amounts to be<br/>
-increased pursuant to subsection (a) are the following:<br/>
-(1) WARTIME DISABILITY COMPENSATION.—Each of the dollar<br/>
-amounts under section 1114 of title 38, United States Code.<br/>
-(2) ADDITIONAL COMPENSATION FOR DEPENDENTS.—Each of<br/>
-the dollar amounts under section 1115(1) of such title.<br/>
-(3) CLOTHING ALLOWANCE.—The dollar amount under sec-<br/>tion 1162 of such title.<br/>
-(4) DEPENDENCY AND INDEMNITY COMPENSATION TO SUR-<br/>
-VIVING SPOUSE.—Each of the dollar amounts under subsections<br/>
-(a) through (d) of section 1311 of such title.<br/>
-(5) DEPENDENCY AND INDEMNITY COMPENSATION TO CHIL-<br/>
-DREN.—Each of the dollar amounts under sections 1313(a) and<br/>
-1314 of such title.<br/>
-(c) DETERMINATION OF INCREASE.—Each dollar amount<br/>
-described in subsection (b) shall be increased by the same percent-<br/>
-age as the percentage by which benefit amounts payable under<br/>
-title II of the Social Security Act (42 U.S.C. 401 et seq.) are<br/>
-increased effective December 1, 2023, as a result of a determination<br/>
-under section 215(i) of such Act (42 U.S.C. 415(i)).</p>
+            </div>
 
-                    </article>
-                </div>
-                <div>
+            <div>
+                <article>
+                    <h3>SEC. 2. INCREASE IN RATES OF DISABILITY COMPENSATION AND<br/>
+                            DEPENDENCY AND INDEMNITY COMPENSATION. </h3>
+                    <p> 38 USC 1114<br/>
+                        note. </p>
+                    <p>
+                        (a) RATE ADJUSTMENT.—Effective on December 1, 2023, the<br/>
+                        Secretary of Veterans Affairs shall increase, in accordance with<br/>
+                        subsection (c), the dollar amounts in effect on November 30, 2023,<br/>
+                        for the payment of disability compensation and dependency and<br/>
+                        indemnity compensation under the provisions specified in subsection<br/>
+                        (b).
+                    </p>
+                    <p>
+                        (b) AMOUNTS TO BE INCREASED.—The dollar amounts to be<br/>
+                        increased pursuant to subsection (a) are the following:<br/>
+                        (1) WARTIME DISABILITY COMPENSATION.—Each of the dollar<br/>
+                        amounts under section 1114 of title 38, United States Code.<br/>
+                        (2) ADDITIONAL COMPENSATION FOR DEPENDENTS.—Each of<br/>
+                        the dollar amounts under section 1115(1) of such title.<br/>
+                        (3) CLOTHING ALLOWANCE.—The dollar amount under sec-<br/>tion 1162 of such title.<br/>
+                        (4) DEPENDENCY AND INDEMNITY COMPENSATION TO SUR-<br/>
+                        VIVING SPOUSE.—Each of the dollar amounts under subsections<br/>
+                        (a) through (d) of section 1311 of such title.<br/>
+                        (5) DEPENDENCY AND INDEMNITY COMPENSATION TO CHIL-<br/>
+                        DREN.—Each of the dollar amounts under sections 1313(a) and<br/>
+                        1314 of such title.<br/>
+                    </p>
+                    <p>
+                        (c) DETERMINATION OF INCREASE.—Each dollar amount<br/>
+                        described in subsection (b) shall be increased by the same percent-<br/>
+                        age as the percentage by which benefit amounts payable under<br/>
+                        title II of the Social Security Act (42 U.S.C. 401 et seq.) are<br/>
+                        increased effective December 1, 2023, as a result of a determination<br/>
+                        under section 215(i) of such Act (42 U.S.C. 415(i)).
+                    </p>
+                </article>
+            </div>
+
+            <div>
+                <section>
                     <article>
                         <p>
                         38 USC 1114<br/>
@@ -73,16 +82,19 @@ under section 215(i) of such Act (42 U.S.C. 415(i)).</p>
                         </p>
                         <p>
                         (d) SPECIAL RULE.—The Secretary of Veterans Affairs may<br/>
-adjust administratively, consistent with the increases made under<br/>
-subsection (a), the rates of disability compensation payable to per-<br/>
-sons under section 10 of Public Law 85–857 (72 Stat. 1263) who 
+                        adjust administratively, consistent with the increases made under<br/>
+                        subsection (a), the rates of disability compensation payable to per-<br/>
+                        sons under section 10 of Public Law 85–857 (72 Stat. 1263) who 
                         </p>
                         <p>PUBLIC LAW 118–6—JUNE 14, 2023 137 STAT. 51 </p>
                         <p>have not received compensation under chapter 11 of title 38, United<br/>
                             States Code. </p>
                     </article>
-                </div>
-                <div>
+                </section>
+            </div>
+
+            <div>
+                <section>
                     <article>
                         <h3>SEC. 3. PUBLICATION OF ADJUSTED RATES. </h3>
                         <p>The Secretary of Veterans Affairs shall publish in the Federal<br/>
@@ -95,10 +107,10 @@ sons under section 10 of Public Law 85–857 (72 Stat. 1263) who
                             note. <br/>
                             Federal Register,<br/>
                             publication. </p>
-                            <p>Approved June 14, 2023.</p >
+                            <p>Approved June 14, 2023.</p>
                     </article>
-                </div>
-            
+                </section>
+            </div>
         </div>
     );
 }
