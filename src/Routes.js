05d39119811f4f07cd54d67e52503e3eeea01bdf
Southen16 <nama.southen16@gmail.com>
2023-09-27 18:53:36 -0700

    updated

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Paper from './Components/Paper';
-import Font from './Components/font';
+
 
 const RouterModule = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/itc505/homework/3" element={<Font />} />
+        <Route path="/itc505/homework/3" element={<Paper />} />
       </Routes>
     </Router>
   );
